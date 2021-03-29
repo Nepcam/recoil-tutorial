041695764056b525bcfbabb567d649989402592d
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-29 19:13:04 +1300

    installed recoil

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  atom,
+  RecoilRoot,
+  // selector,
+  useRecoilState,
+  // useRecoilValue,
+} from 'recoil';
+
+const usernameState = atom({
+  key: 'username',
+  default: 'Red',
+});
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <RecoilRoot>
+      <Nav />
+      <Body />
+    </RecoilRoot>
+  );
+}
+
+function Nav() {
+  return <div className='nav'></div>;
+}
+
+function Body() {
+  return (
+    <div className='body'>
+      <Profile />
+    </div>
+  );
+}
+
+function Profile() {
+  const [username, setUsername] = useRecoilState(usernameState);
+  return (
+    <div>
+      <h2>Profile:</h2>
+      <p>{username}</p>
+      <input
+        type='text'
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
